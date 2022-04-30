596a00c50838b730c7f14bc97a7639e8955866d9
poljik <poljik@gmail.com>
2022-04-30 23:19:10 +0300

    add task mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let width = matrix[0].length;
+  let height = matrix.length;
+  let arr = [];
+  for (let index = 0; index < height; index++) {
+    arr[index] = [];
+    for (let index2 = 0; index2 < width; index2++) {
+      arr[index][index2] = 0;
+    }
+  }
+  for (let index = 0; index < height; index++) {
+    for (let index2 = 0; index2 < width; index2++) {
+      if (matrix[index][index2]) {
+        if (index-1 >= 0) {
+          arr[index-1][index2] += 1;//up
+        }
+        if (index+1 <= height) {
+          arr[index+1][index2] += 1;//down
+        }
+        if (index2+1 <= width) {
+          arr[index][index2+1] += 1;//right
+        }
+        if (index2-1 >= 0) {
+          arr[index][index2-1] += 1;//left
+        }
+        if (index-1 >= 0 && index2+1 <= width) {
+          arr[index-1][index2+1] += 1;//up-right
+        }
+        if (index-1 >= 0 && index2-1 <= 0) {
+          arr[index-1][index2-1] += 1;//up-left
+        }
+        if (index+1 <= height && index2+1 <= width) {
+          arr[index+1][index2+1] += 1;//down-right
+        }
+        if (index+1 <= height && index2-1 >= 0) {
+          arr[index+1][index2-1] += 1;//down-left
+        }
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = {
