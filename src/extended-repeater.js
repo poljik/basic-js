68c3f25d195f7f8e0ede18342528ffef39f3108a
poljik <poljik@gmail.com>
2022-04-30 18:08:51 +0300

    add task repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (Object.prototype.toString.call(str) === '[object Null]') {
+     str = 'null';
+  }
+  str = `${str}`;
+  let repeatTimes = 1;
+  if (arguments[1].repeatTimes) {
+    repeatTimes = arguments[1].repeatTimes;
+  }
+  let separator = '+';
+  if (arguments[1].separator) {
+    separator = arguments[1].separator;
+  }
+  
+  let addition = '';
+  if (arguments[1].addition || Object.prototype.toString.call(arguments[1].addition) === '[object Boolean]' ) {
+    addition = arguments[1].addition;
+  }
+  if (Object.prototype.toString.call(arguments[1].addition) === '[object Null]') {
+    addition = 'null';
+  }
+  addition = `${addition}`;
+  let additionRepeatTimes = 1;
+  if (arguments[1].additionRepeatTimes) {
+    additionRepeatTimes = arguments[1].additionRepeatTimes;
+  }
+  let additionSeparator = '|';
+  if (arguments[1].additionSeparator) {
+    additionSeparator = arguments[1].additionSeparator;
+  }
+  
+  let res = '';
+  for (let index = 0; index < repeatTimes; index++) {
+    res += str;
+    for (let index2 = 0; index2 < additionRepeatTimes; index2++) {
+      if (addition) {
+        res += addition;
+      }
+      if (index2 !== (additionRepeatTimes-1)) {
+        res += additionSeparator;
+      }
+    }
+    if (index !== (repeatTimes-1)) res += separator;
+  }
+  return res;
 }
 
 module.exports = {
